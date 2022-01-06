98fb5052be416a8c069b4a1f7a18c38332917c56
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-01-06 16:57:10 -0600

    initial commit

---
+++ Pocket Weather/View/WeatherCardView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherCardView.swift
+//  Pocket Weather
+//
+//  Created by Geoffrey Johnson on 1/6/22.
+//
+
+import SwiftUI
+
+struct WeatherCardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct WeatherCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherCardView()
+    }
+}
