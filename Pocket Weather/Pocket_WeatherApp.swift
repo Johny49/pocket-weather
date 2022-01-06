f0a9f1000d81fac30faef01ab716bf226a94716a
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2022-01-06 08:45:16 -0600

    Initial Commit

---
+++ Pocket Weather/Pocket_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_WeatherApp.swift
+//  Pocket Weather
+//
+//  Created by Geoffrey Johnson on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
